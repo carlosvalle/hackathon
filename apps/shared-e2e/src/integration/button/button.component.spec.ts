16cb112ecc2803fd7281f95de95844a02d502d36
Carlos Valle <carlos.m.valle@gmail.com>
2021-10-08 23:09:43 -0400

    chore: remove unused shared e2e

--- apps/shared-e2e/src/integration/button/button.component.spec.ts
+++
@@ -1,6 +0,0 @@
-describe('shared', () => {
-  beforeEach(() => cy.visit('/iframe.html?id=buttoncomponent--primary'));
-  it('should render the component', () => {
-    cy.get('hackathon-button').should('exist');
-  });
-});
